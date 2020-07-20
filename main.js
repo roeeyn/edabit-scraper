5605eb4469c885b9db4cea448870a360fe719fd4
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-07-19 19:23:23 -0500

    Finished scrapping logic. First massive run

---
+++ main.js
@@ -0,0 +1,26 @@
+const fsp = require("fs/promises");
+const exerciseList = require("./exercises_list.json");
+const { main } = require("./getCompleteExercise");
+const reduce = require("awaity/reduce");
+const mainScript = async () => {
+  const finalExercises = await reduce.default(
+    exerciseList.filter((a, i) => i < 5),
+    async (carry, { url }, iteration, originalArray) => {
+      console.log(`********************* ${url} *************************`);
+      console.log(`******** ${iteration + 1}/${originalArray.length} ******`);
+      const exerciseDetails = await main(url);
+      console.log(exerciseDetails);
+      const newCarry = [...carry, exerciseDetails];
+      const jsonifiedCarry = JSON.stringify(newCarry, null, "\t");
+      await fsp.writeFile("./complete_carry.json", jsonifiedCarry);
+      return newCarry;
+    },
+    []
+  );
+
+  const jsonifiedResult = JSON.stringify(finalExercises, null, "\t");
+  await fsp.writeFile("./complete_exercises.json", jsonifiedResult);
+  console.log("ahuevo...?");
+};
+
+mainScript();

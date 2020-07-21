ee58c300896a5f60f0b50a1a7f060b8e15ca75e0
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-07-20 21:08:24 -0500

    Added merged list script and demo file

---
+++ mergeLists.js
@@ -0,0 +1,22 @@
+const completeExercises = require("./ut_complete_exercises.json");
+const exercisesList = require("./ut_exercises_list.json");
+const fsp = require("fs/promises");
+
+const unmatchedElements = completeExercises.filter(
+  (element, idx) => element.url !== exercisesList[idx].url
+);
+
+if (unmatchedElements.length > 0) {
+  console.log(
+    "Lists from ut_complete_exercises.json and ut_exercises_list.json must match"
+  );
+  return;
+}
+
+const mergedList = completeExercises.map((element, idx) => {
+  return { ...element, ...exercisesList[idx] };
+});
+
+const jsonifiedMergeList = JSON.stringify(mergedList, null, "\t");
+fsp.writeFile("./ut_merged_list.json", jsonifiedMergeList);
+console.log(`Written ${mergedList.length} challenges`);

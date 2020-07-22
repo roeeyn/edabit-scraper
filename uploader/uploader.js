2e5e956b6fc644446132b183112047af29d5ac16
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-07-21 21:13:24 -0500

    Finished uploader script

--- uploader/uploader.js
+++ uploader/uploader.js
@@ -2,8 +2,26 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const { connect2db } = require("./db/db");
+const exerciseList = require("../ut_merged_list.json");
+const { Exercise } = require("./db/models");
+const fsp = require("fs/promises");
+const { each } = require("awaity/esm");
 
-console.log(process.env.DB_HOST);
 connect2db()
-  .then(message => console.log("holi", message))
+  .then(async message => {
+    console.log(message);
+
+    await each(exerciseList, async (exercise, idx) => {
+      try {
+        const newExercise = new Exercise({ ...exercise });
+        await newExercise.save();
+        console.log(`Saved ${idx + 1}/${exerciseList.length}`);
+      } catch (error) {
+        console.log(error);
+        fsp.appendFile("./errors.txt", `${exercise.url}\n`);
+      }
+    });
+
+    console.log("Finished Successfuly");
+  })
   .catch(err => console.error("holi", err));

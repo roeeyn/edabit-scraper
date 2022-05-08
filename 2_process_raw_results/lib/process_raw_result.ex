09a34bf2c574b4e03c06f2b5f7362b1f78077622
Rodrigo Medina <rodrigom@lyft.com>
2022-05-08 17:58:57 -0500

    Added process raw results app template

---
+++ 2_process_raw_results/lib/process_raw_result.ex
@@ -0,0 +1,18 @@
+defmodule ProcessRawResult do
+  @moduledoc """
+  Documentation for `ProcessRawResult`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProcessRawResult.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2_process_raw_results/test/process_raw_result_test.exs
@@ -0,0 +1,8 @@
+defmodule ProcessRawResultTest do
+  use ExUnit.Case
+  doctest ProcessRawResult
+
+  test "greets the world" do
+    assert ProcessRawResult.hello() == :world
+  end
+end

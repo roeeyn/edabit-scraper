26e1ca9c8831b43c5929df834bce6ff7396f91f3
Rodrigo Medina <rodrigom@lyft.com>
2022-05-08 12:07:22 -0500

    Tried elixir websockets, failed due to auth

--- lib/edabit_scrapper.ex
+++ lib/edabit_scrapper.ex
@@ -2,6 +2,7 @@ defmodule EdabitScrapper do
   @moduledoc """
   Documentation for `EdabitScrapper`.
   """
+  alias EdabitScrapper.Impl.WsScrapper
 
   @doc """
   Hello world.
@@ -15,4 +16,8 @@ defmodule EdabitScrapper do
   def hello do
     :world
   end
+
+  defdelegate create(), to: WsScrapper, as: :start_link
+
+  defdelegate send_demo(pid), to: WsScrapper, as: :send_demo
 end

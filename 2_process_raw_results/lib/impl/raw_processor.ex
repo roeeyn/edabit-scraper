cfa9f94b4782ca6319d2213a016340611ff89fef
Rodrigo Medina <rodrigom@lyft.com>
2022-05-08 20:56:40 -0500

    Refactored main function

--- 2_process_raw_results/lib/impl/raw_processor.ex
+++ 2_process_raw_results/lib/impl/raw_processor.ex
@@ -1,7 +1,6 @@
 defmodule ProcessRawResult.Impl.RawProcessor do
-  def get_payload_from_result(result_file_path) do
-    with {:ok, readed_file} <- File.read(result_file_path),
-         {:ok, decoded_raw_file} <- Jason.decode(readed_file),
+  def get_payload_from_result(readed_file) do
+    with {:ok, decoded_raw_file} <- Jason.decode(readed_file),
          temp <- String.slice(decoded_raw_file["payload"], 1..-1),
          {:ok, message_list} = Jason.decode(temp),
          [message | _] <- message_list,

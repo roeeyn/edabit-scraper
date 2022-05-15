b9d5d11b33720266bbbb7388889c3e8fefdff1a3
Rodrigo Medina <rodrigom@lyft.com>
2022-05-14 21:22:51 -0500

    Added new module for challenge details

---
+++ 3_get_challenges_details/lib/get_challenges_details.ex
@@ -0,0 +1,18 @@
+defmodule GetChallengesDetails do
+  @moduledoc """
+  Documentation for `GetChallengesDetails`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GetChallengesDetails.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 3_get_challenges_details/test/get_challenges_details_test.exs
@@ -0,0 +1,8 @@
+defmodule GetChallengesDetailsTest do
+  use ExUnit.Case
+  doctest GetChallengesDetails
+
+  test "greets the world" do
+    assert GetChallengesDetails.hello() == :world
+  end
+end

83c238e5c5c29d4367d578b0c599907fe0ab7daf
Rodrigo Medina <rodrigom@lyft.com>
2022-05-14 23:41:00 -0500

    WIP: Added raw processor scaffolding

---
+++ 4_process_raw_challenge_data_response/lib/impl/raw_processor.ex
@@ -0,0 +1,7 @@
+defmodule ProcessRawChallengeDataResponse.Impl.RawProcessor do
+
+  def get_messages_from_raw_result(raw_result) do
+
+  end
+
+end
--- 4_process_raw_challenge_data_response/lib/process_raw_challenge_data_response.ex
+++ 4_process_raw_challenge_data_response/lib/process_raw_challenge_data_response.ex
@@ -1,18 +1,23 @@
 defmodule ProcessRawChallengeDataResponse do
   @moduledoc """
-  Documentation for `ProcessRawChallengeDataResponse`.
+  This module process the websocket list of responses obtained from
+  the ZAP API, which contains the exercises data. We intend to format
+  this messages as json, so we can use this files later for easier further processing.
   """
+  alias ProcessRawChallengeDataResponse.Impl.RawProcessor
+  defdelegate get_messages_from_raw_result(raw_result), to: RawProcessor
 
-  @doc """
-  Hello world.
+  # defdelegate get_payload_from_raw_result(result_file_path), to: RawProcessor
+  # defdelegate get_payload_from_raw_result!(result_file_path), to: RawProcessor
+  # defdelegate write_payload_to_json_file(processed_payload), to: RawProcessor
+  # defdelegate write_payload_to_json_file(processed_payload, output_file), to: RawProcessor
 
-  ## Examples
-
-      iex> ProcessRawChallengeDataResponse.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  # @doc """
+  # This is the main process of transforming the raw result message to a json file.
+  # """
+  # def raw_result_to_json(result_file_path) do
+  #   File.read!(result_file_path)
+  #   |> ProcessRawResult.get_payload_from_raw_result!()
+  #   |> ProcessRawResult.write_payload_to_json_file("#{result_file_path}.json")
+  # end
 end

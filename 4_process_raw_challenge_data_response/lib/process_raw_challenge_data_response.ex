b47f5bee7dfd5f2748388baeb96a57080238e19d
Rodrigo Medina <rodrigom@lyft.com>
2022-05-14 23:15:29 -0500

    Adding new mix project for processing data results

---
+++ 4_process_raw_challenge_data_response/lib/process_raw_challenge_data_response.ex
@@ -0,0 +1,18 @@
+defmodule ProcessRawChallengeDataResponse do
+  @moduledoc """
+  Documentation for `ProcessRawChallengeDataResponse`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProcessRawChallengeDataResponse.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

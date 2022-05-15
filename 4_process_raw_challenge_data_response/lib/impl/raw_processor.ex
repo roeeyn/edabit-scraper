a88da01d12c094953ed342621424c8dad682cd39
Rodrigo Medina <rodrigom@lyft.com>
2022-05-15 12:00:21 -0500

    Implemented the raw processor

--- 4_process_raw_challenge_data_response/lib/impl/raw_processor.ex
+++ 4_process_raw_challenge_data_response/lib/impl/raw_processor.ex
@@ -1,7 +1,33 @@
 defmodule ProcessRawChallengeDataResponse.Impl.RawProcessor do
-
-  def get_messages_from_raw_result(raw_result) do
-
+  def get_message_from_raw_result(raw_result) do
+    with temp <- String.slice(raw_result, 1..-1),
+         {:ok, payload_list} <- Jason.decode(temp),
+         raw_payload <- Enum.at(payload_list, 0),
+         {:ok, payload} <- Jason.decode(raw_payload) do
+      {:ok, payload}
+    else
+      _ -> {:error, "Failed to parse raw result"}
+    end
   end
 
+  def get_message_from_raw_result!(raw_result) do
+    with temp <- String.slice(raw_result, 1..-1),
+         {:ok, payload_list} <- Jason.decode(temp),
+         raw_payload <- Enum.at(payload_list, 0),
+         {:ok, payload} <- Jason.decode(raw_payload) do
+      payload
+    end
+  end
+
+  def write_payload_to_json_file(processed_payload) do
+    {:ok, encoded_payload} = Jason.encode(processed_payload)
+    formatted_file = Jason.Formatter.pretty_print(encoded_payload)
+    File.write("processed_result.json", formatted_file, [:binary])
+  end
+
+  def write_payload_to_json_file(processed_payload, output_file) do
+    {:ok, encoded_payload} = Jason.encode(processed_payload)
+    formatted_file = Jason.Formatter.pretty_print(encoded_payload)
+    File.write(output_file, formatted_file, [:binary])
+  end
 end
--- 4_process_raw_challenge_data_response/lib/process_raw_challenge_data_response.ex
+++ 4_process_raw_challenge_data_response/lib/process_raw_challenge_data_response.ex
@@ -5,19 +5,23 @@ defmodule ProcessRawChallengeDataResponse do
   this messages as json, so we can use this files later for easier further processing.
   """
   alias ProcessRawChallengeDataResponse.Impl.RawProcessor
-  defdelegate get_messages_from_raw_result(raw_result), to: RawProcessor
+  defdelegate get_message_from_raw_result(raw_result), to: RawProcessor
+  defdelegate get_message_from_raw_result!(raw_result), to: RawProcessor
 
-  # defdelegate get_payload_from_raw_result(result_file_path), to: RawProcessor
-  # defdelegate get_payload_from_raw_result!(result_file_path), to: RawProcessor
-  # defdelegate write_payload_to_json_file(processed_payload), to: RawProcessor
-  # defdelegate write_payload_to_json_file(processed_payload, output_file), to: RawProcessor
+  defdelegate write_payload_to_json_file(processed_payload), to: RawProcessor
+  defdelegate write_payload_to_json_file(processed_payload, output_file), to: RawProcessor
 
-  # @doc """
-  # This is the main process of transforming the raw result message to a json file.
-  # """
-  # def raw_result_to_json(result_file_path) do
-  #   File.read!(result_file_path)
-  #   |> ProcessRawResult.get_payload_from_raw_result!()
-  #   |> ProcessRawResult.write_payload_to_json_file("#{result_file_path}.json")
-  # end
+  @doc """
+  This is the main process of transforming the raw result messages from a folder to a json file.
+  """
+  def raw_results_to_json(folder_name) do
+    Path.wildcard("assets/#{folder_name}/*.txt")
+    |> Enum.map(&File.read!/1)
+    |> Enum.map(&ProcessRawChallengeDataResponse.get_message_from_raw_result!/1)
+    |> Enum.filter(fn payload -> !is_list(payload["result"]) end)
+    |> Enum.map(fn payload -> payload["result"] end)
+    |> ProcessRawChallengeDataResponse.write_payload_to_json_file(
+      "assets/#{folder_name}/#{folder_name}_challenges_data_details.json"
+    )
+  end
 end

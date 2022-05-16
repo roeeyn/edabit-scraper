280a18d4d3bccc5eac963e6d2d84356db7338746
Rodrigo Medina <rodrigom@lyft.com>
2022-05-15 21:59:39 -0500

    Initial mix project

---
+++ 5_upload_challenges_to_mongo/lib/upload_challenges_to_mongo.ex
@@ -0,0 +1,18 @@
+defmodule UploadChallengesToMongo do
+  @moduledoc """
+  Documentation for `UploadChallengesToMongo`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UploadChallengesToMongo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 5_upload_challenges_to_mongo/test/upload_challenges_to_mongo_test.exs
@@ -0,0 +1,8 @@
+defmodule UploadChallengesToMongoTest do
+  use ExUnit.Case
+  doctest UploadChallengesToMongo
+
+  test "greets the world" do
+    assert UploadChallengesToMongo.hello() == :world
+  end
+end

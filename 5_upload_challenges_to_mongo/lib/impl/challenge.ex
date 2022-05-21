fd3d9e6553a7e15052e5b73f84d1ac4f40b1fec4
Rodrigo Medina <rodrigom@lyft.com>
2022-05-21 16:58:02 -0500

    Created transform from edabit

--- 5_upload_challenges_to_mongo/lib/impl/challenge.ex
+++ 5_upload_challenges_to_mongo/lib/impl/challenge.ex
@@ -21,6 +21,22 @@ defmodule MongoChallengesUploader.Impl.Challenge do
     attribute(:title, String.t())
   end
 
+  def create_from_edabit_challenge(edabit_challenge) do
+    Challenge.new()
+    |> Map.put(:edabit_id, edabit_challenge._id)
+    |> Map.put(:author, edabit_challenge.author)
+    |> Map.put(:author_edabit_id, edabit_challenge.author_id)
+    |> Map.put(:raw_code, edabit_challenge.code)
+    |> Map.put(:difficulty, edabit_challenge.difficulty)
+    |> Map.put(:raw_instructions, edabit_challenge.instructions)
+    |> Map.put(:raw_tests, edabit_challenge.lab)
+    |> Map.put(:programming_language, edabit_challenge.language)
+    |> Map.put(:quality, edabit_challenge.quality)
+    |> Map.put(:summary, edabit_challenge.summary)
+    |> Map.put(:tags, edabit_challenge.tags)
+    |> Map.put(:title, edabit_challenge.title)
+  end
+
   def insert_one(%Challenge{} = todo) do
     with map <- dump(todo),
          {:ok, _} <- Mongo.insert_one(:mongo, @collection, map) do

fa0f9fa3dbcbe5d34e0f39d8226878ac35bd96d9
Rodrigo Medina <rodrigom@lyft.com>
2022-05-21 16:10:57 -0500

    added jason dependency

--- 5_upload_challenges_to_mongo/mix.exs
+++ 5_upload_challenges_to_mongo/mix.exs
@@ -22,7 +22,8 @@ defmodule MongoChallengesUploader.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mongodb_driver, "~> 0.9.0"}
+      {:mongodb_driver, "~> 0.9.0"},
+      {:jason, "~> 1.3"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
